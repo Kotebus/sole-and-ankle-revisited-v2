764b6372e37154610da0bd15f894c7e15ea3e48b
Kotebus666 <kotebus666@gmail.com>
2025-07-26 16:59:08 +0300

    Exercise 3: Tweaks to our main view

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,22 +3,33 @@ import styled from 'styled-components';
 
 import Header from '../Header';
 import ShoeIndex from '../ShoeIndex';
+import {QUERIES} from "../../constants.js";
 
 const App = () => {
-  const [sortId, setSortId] = React.useState('newest');
+    const [sortId, setSortId] = React.useState('newest');
 
-  return (
-    <>
-      <Header />
-      <Main>
-        <ShoeIndex sortId={sortId} setSortId={setSortId} />
-      </Main>
-    </>
-  );
+    return (
+        <>
+            <Header/>
+            <Main>
+                <ShoeIndex sortId={sortId} setSortId={setSortId}/>
+            </Main>
+        </>
+    );
 };
 
 const Main = styled.main`
-  padding: 64px 32px;
+    padding: 64px 32px;
+
+    @media (${QUERIES.tabletAndSmaller}) {
+        padding-top: 48px;
+        padding-bottom: 48px;
+    }
+
+    @media (${QUERIES.mobile}) {
+        padding-left: 16px;
+        padding-right: 16px;
+    }
 `;
 
 export default App;
--- src/components/ShoeIndex/ShoeIndex.jsx
+++ src/components/ShoeIndex/ShoeIndex.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { WEIGHTS } from '../../constants';
+import {QUERIES, WEIGHTS} from '../../constants';
 
 import Breadcrumbs from '../Breadcrumbs';
 import Select from '../Select';
@@ -35,7 +35,7 @@ const ShoeIndex = ({ sortId, setSortId }) => {
             Shoes
           </Breadcrumbs.Crumb>
         </Breadcrumbs>
-        <Spacer size={42} />
+        <LeftColumnSpacer size={42} />
         <ShoeSidebar />
       </LeftColumn>
     </Wrapper>
@@ -47,10 +47,30 @@ const Wrapper = styled.div`
   flex-direction: row-reverse;
   align-items: baseline;
   gap: 32px;
+  
+  @media (${QUERIES.tabletAndSmaller}) {
+    flex-direction: column-reverse;  
+    gap: 0;
+  }
+`;
+
+const LeftColumnSpacer = styled(Spacer)`
+  @media (${QUERIES.tabletAndSmaller}) {
+    display: none;
+  }
 `;
 
 const LeftColumn = styled.div`
   flex-basis: 248px;
+
+  @media (${QUERIES.tabletAndSmaller}) {
+    flex-basis: 0;
+    margin-bottom: -0.5rem;
+  }
+  
+  @media (${QUERIES.mobile}) {
+    margin-bottom: 0.25rem;
+  }
 `;
 
 const MainColumn = styled.div`
@@ -61,6 +81,13 @@ const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: baseline;
+
+  @media (${QUERIES.tabletAndSmaller}) {
+    align-items: flex-end;
+  }
+  @media (${QUERIES.mobile}) {
+    align-items: flex-end;
+  }
 `;
 
 const Title = styled.h2`
